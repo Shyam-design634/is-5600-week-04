5cb023b8aa4f8ccbcc448cfe6b90718e7e2f470c
Shyam-design634 <meghashyamdora.hanumanthu@slu.edu>
2024-11-19 06:51:50 +0000

    Committed changes to app.js

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const path = require('path');
 const api = require('./api'); // Import route handlers from api.js
+const middleware = require('./middleware');
 
 // Set up the app and port
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Use the CORS middleware
+app.use(middleware.cors);
 
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
